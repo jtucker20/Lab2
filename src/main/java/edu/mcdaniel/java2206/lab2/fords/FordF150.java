7fa868b616e3c8f63c4f4a4414a95d84bec11193
joafa <ja017@mcdaniel.edu>
2020-02-24 20:41:22 -0500

    Lab2_Step1

---
+++ src/main/java/edu/mcdaniel/java2206/lab2/fords/FordF150.java
@@ -0,0 +1,66 @@
+package edu.mcdaniel.java2206.lab2.fords;
+
+import edu.mcdaniel.java2206.lab2.interfaces.Vehicle;
+import edu.mcdaniel.java2206.lab2.vehicles.FordVehicle;
+
+public class FordF150 extends FordVehicle implements Vehicle {
+
+
+    //Private Assets
+    private double accelerationRate;
+    private double distance;
+
+    private boolean lightsOn;
+
+    //Constructors
+    public FordF150(){
+        super();
+        setModel("Camry");
+        this.lightsOn = false;
+    }
+
+
+    @Override
+    public void accel(double percentOfMaxAccel) {
+
+    }
+
+    @Override
+    public void brake(double percentOfMaxBrake) {
+
+    }
+
+    @Override
+    public void turnOnLights() {
+
+    }
+
+    @Override
+    public void toggleLights() {
+
+    }
+
+    @Override
+    public String showLightsStatus() {
+        return null;
+    }
+
+    @Override
+    public String showVehicleState() {
+        return "This vehicle has an acceleration of: " + this.accelerationRate
+                + "\nThis vehicle has it's lights " + ((this.lightsOn) ? "On" : "Off");
+    }
+
+    //Getters
+    public double getAccelerationRate() {
+        return accelerationRate;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public boolean isLightsOn() {
+        return lightsOn;
+    }
+}

9c1646311d3ff504b3c94b446d83d4f6bb8cb567
abby <arg0102@mcdaniel.edu>
2020-02-26 17:07:37 -0500

    In progress.

--- src/test/java/edu/mcdaniel/java2206/lab2/ApplicationTests.java
+++ src/test/java/edu/mcdaniel/java2206/lab2/ApplicationTests.java
@@ -1,18 +1,33 @@
 package edu.mcdaniel.java2206.lab2;
 
+import edu.mcdaniel.java2206.lab2.toyotas.ToyotaCamry;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationTests {
 
+	private ToyotaCamry ToyotaCamryClass;
 
+	@BeforeEach
+	void setUp()
+	{
+
+		ToyotaCamryClass = new ToyotaCamry();
+
+	}
 
 	@Test
-	void contextLoads() {
-		//We are testing to see if the spring boot application loads.
-		// This will fail only if the spring boot
-		// application fails to load. No need to add functionality here.
+	void accel()
+	{
+		//Arrange
+
+		//Act
+
+		//Assert
+
+
 	}
 
 }

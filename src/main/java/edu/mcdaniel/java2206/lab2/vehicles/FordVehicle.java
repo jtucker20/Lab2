10016024d778cf6e23e89cf0f2d01bd5c0c09f96
joafa <ja017@mcdaniel.edu>
2020-02-26 15:17:57 -0500

    Lab2_Step2

--- src/main/java/edu/mcdaniel/java2206/lab2/vehicles/FordVehicle.java
+++ src/main/java/edu/mcdaniel/java2206/lab2/vehicles/FordVehicle.java
@@ -3,8 +3,21 @@ package edu.mcdaniel.java2206.lab2.vehicles;
 import edu.mcdaniel.java2206.lab2.car.CarBaseClass;
 
 public class FordVehicle extends CarBaseClass {
+    private double speed;
 
-    public FordVehicle(){
+    public FordVehicle() {
+            super();
+            speed = 0;
         setMake("Ford");
+    }
+    public String displaySpeedAsString(){
+        return "This Toyota's speed is: " + speed;
+    }
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
     }
 }

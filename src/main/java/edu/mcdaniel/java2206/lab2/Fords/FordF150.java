1ea1b20775fe399f2431c84515a091ff50c41867
inesagasaro <ika001@mcdaniel.edu>
2020-02-26 19:17:47 -0500

    working on lab2

---
+++ src/main/java/edu/mcdaniel/java2206/lab2/Fords/FordF150.java
@@ -0,0 +1,72 @@
+package edu.mcdaniel.java2206.lab2.Fords;
+
+import edu.mcdaniel.java2206.lab2.interfaces.Vehicle;
+import edu.mcdaniel.java2206.lab2.vehicles.FordVehicle;
+
+public class FordF150 extends FordVehicle implements Vehicle {
+
+    //Private Assets
+    private double accelerationRate;
+    private double distance;
+
+    private boolean lightsOn;
+
+    //Constructors
+    public FordF150(){
+        super();
+        setModel("Ford");
+        this.lightsOn = false;
+    }
+
+    //Major Methods
+    @Override
+    public void accel(double percentOfMaxAccel) {
+        this.accelerationRate = percentOfMaxAccel;
+    }
+
+    @Override
+    public void brake(double percentOfMaxBrake) {
+        this.accelerationRate = this.accelerationRate - percentOfMaxBrake;
+    }
+
+    @Override
+    public void turnOnLights() {
+        this.lightsOn = true;
+    }
+
+    @Override
+    public void toggleLights() {
+        this.lightsOn = !this.lightsOn;
+    }
+
+    @Override
+    public String showLightsStatus() {
+        return "The Lights are " + ( (this.lightsOn) ? "On" : "Off");
+    }
+
+    @Override
+    public String showVehicleState() {
+        return "This vehicle has an acceleration of: " + this.accelerationRate
+                + "\nThis vehicle has it's lights " + ((this.lightsOn) ? "On" : "Off");
+    }
+
+    //Getters
+    public double getAccelerationRate() {
+        return accelerationRate;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public boolean isLightsOn() {
+        return lightsOn;
+    }
+}
+
+
+
+
+
+
+
--- src/main/java/edu/mcdaniel/java2206/lab2/vehicles/FordVehicle.java
+++ src/main/java/edu/mcdaniel/java2206/lab2/vehicles/FordVehicle.java
@@ -4,7 +4,24 @@ import edu.mcdaniel.java2206.lab2.car.CarBaseClass;
 
 public class FordVehicle extends CarBaseClass {
 
+    private double speed;
+
     public FordVehicle(){
+        super();
         setMake("Ford");
+        speed = 0;
+    }
+
+    public String displaySpeedAsString(){
+        return "This Ford's speed is: " + speed;
+    }
+
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
     }
 }

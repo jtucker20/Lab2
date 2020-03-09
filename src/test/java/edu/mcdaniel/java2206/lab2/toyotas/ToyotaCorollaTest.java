bc75b747710c0c45f6fda3080b2037ce23b023df
inesagasaro <ika001@mcdaniel.edu>
2020-03-09 19:48:47 -0400

    working on lab2

---
+++ src/test/java/edu/mcdaniel/java2206/lab2/toyotas/ToyotaCorollaTest.java
@@ -0,0 +1,32 @@
+package edu.mcdaniel.java2206.lab2.toyotas;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ToyotaCorollaTest {
+
+    @Test
+    void accel() {
+    }
+
+    @Test
+    void brake() {
+    }
+
+    @Test
+    void turnOnLights() {
+    }
+
+    @Test
+    void toggleLights() {
+    }
+
+    @Test
+    void showLightsStatus() {
+    }
+
+    @Test
+    void showVehicleState() {
+    }
+}
